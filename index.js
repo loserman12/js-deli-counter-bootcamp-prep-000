b73314761846b84e44fd522798aa692c406a96a8
ralphcuello1016@gmail.com <ralphcuello1016@gmail.com>
2018-05-26 22:48:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ function nowServing(katzDeli){
 }
 }
 function currentLine(katzDeli){
+  var line = [];
   for (let i=0; i < katzDeli.length; i++){
-    katzDeli.push(` `+[i+1]+`. ` + katzDeli[i])
+    line.push(` `+[i+1]+`. ` + katzDeli[i])
   
   }
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
-  } else return (`The line is currently:` + katzDeli);
+  } else return (`The line is currently:` + line);
   
 }
